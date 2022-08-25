630c22d259f35e8407aca07a1b33dd1e69952c5f
jmachuca131281 <jmachucaprogrammer@gmail.com>
2022-08-25 15:25:33 -0500

    Se actualiza el repositorio con las mejoras en código.

--- codigo_facilito/server/src/main.go
+++ codigo_facilito/server/src/main.go
@@ -18,32 +18,34 @@ const password string = "R00t"
 const database string = "taller1"
 
 type User struct {
-	Id int `json:"id"`
-	Username string `json:"username"`
+	Id         int    `json:"id"`
+	Username   string `json:"username"`
 	First_name string `json:"first_name"`
-	Last_name string `json:"last_name"`
+	Last_name  string `json:"last_name"`
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Data User `json:"data"`
+	Status  string `json:"status"`
+	Data    User   `json:"data"`
 	Message string `json:"message"`
 }
 
-func Getuser(w http.ResponseWriter, r* http.Request){  // Asociamos func a url
-	vars := mux.Vars(r)
-	user_id := vars["id"]
+func GetUserRegisterRequest(w http.ResponseWriter, r* http.Request){  // Asociamos func a url
+	vars := mux.Vars(r)                                // Vars returns the route variables for the current request, if any. Return Map
+	user_id := vars["id"]                              // Separamos el id
+
 	status := "success"
-	var message string
-	user := GetUser(user_id)
+	message := "Return user"
+
+	user := GetUserQuery(user_id)
 
 	if user.Id <= 0 {
-		status = "error"
+		status = "ERROR INVALID_ID"
 		message = "User not found"
 	}
 
 	response := Response{status, user, message}
-	json.NewEncoder(w).Encode(response)
+	json.NewEncoder(w).Encode(response)   				 // NewEncoder returns a new encoder that writes to w.
 }
 
 func InitalizeDatabase() {
@@ -51,9 +53,8 @@ func InitalizeDatabase() {
 	log.Println("Conexión DB exitosa")
 }
 
-func CloseConnection() {
-	connection.Close()
-	log.Println("Conexión db cerrada")
+func CreateString() string {
+	return username + ":" + password + "@/" + database
 }
 
 func ConnectORM(stringConnection string) *gorm.DB {
@@ -65,24 +66,26 @@ func ConnectORM(stringConnection string) *gorm.DB {
 	return connection
 }
 
-func GetUser(id string) User {
+func CloseConnection() {
+	connection.Close()
+	log.Println("Conexión DB cerrada")
+}
+
+
+func GetUserQuery(id string) User {
 	user := User{}
-	connection.Where("id=?", id).First(&user)
+	connection.Where("id = ?", id).First(&user)                // Return a new relation
 	return user
 }
 
-func CreateString() string {
-	return username + ":" + password + "@/" + database
-}
-
-func NewUser(w http.ResponseWriter, r* http.Request) {
+func NewUserRequest(w http.ResponseWriter, r* http.Request) {
 	user := GetUserRequest(r)
-	response := Response{"success", CreateUser(user), ""}
+	response := Response{"success", CreateUser(user), "User is created"}
 	json.NewEncoder(w).Encode(response)
 }
 
 func GetUserRequest(r* http.Request) User {
-	var user = User{}
+	user := User{}
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&user)
 	if err != nil {
@@ -96,51 +99,56 @@ func CreateUser(user User)  User{
 	return user
 }
 
-func Updateuser(w http.ResponseWriter, r* http.Request) {
+func UpdateUserRequest(w http.ResponseWriter, r* http.Request) {
 	vars := mux.Vars(r)
 	user_id := vars["id"]	
 	user := GetUserRequest(r)
-	response := Response{"success", UpdateUser(user_id, user), ""}
-	UpdateUser(user_id, user)
+	response := Response{"success", UpdateUser(user_id, user), "User update correctly"}
 	json.NewEncoder(w).Encode(response)
 }
 
 func UpdateUser(id string, user User) User {
 	currentUser := User{}
-	connection.Where("id=?", id).First(&currentUser)
+	connection.Where("id = ?", id).First(&currentUser)                  // Query to DB
+
 	currentUser.Username = user.Username
 	currentUser.First_name = user.First_name 
 	currentUser.Last_name = user.Last_name
+
 	connection.Save(&currentUser)
+	
 	return currentUser
 }
 
-func Deleteuser(w http.ResponseWriter, r* http.Request) {
+func DeleteUserRequest(w http.ResponseWriter, r* http.Request) {
 	vars := mux.Vars(r)
 	user_id := vars["id"]
+
 	var user User
 	DeleteUser(user_id)
-	response := Response{"succes", user, ""}
+	response := Response{"succes", user, "Register eliminated"}
 	json.NewEncoder(w).Encode(response)
 }
 
 func DeleteUser(id string) {
 	user := User{}
-	connection.Where("id = ?", id).First(&user)
+	connection.Where("id = ?", id).First(&user)       // Query to db
 	connection.Delete(&user)
 }
 
 func main() {
 	InitalizeDatabase()
 	defer CloseConnection()
+
+	// Register URL paths and handlers
 	r := mux.NewRouter() 							                // Permite crear una intancia de NewRouter
 
-	r.HandleFunc("/user/{id}", Getuser).Methods("GET")              // Declaramos el usuario
-	r.HandleFunc("/user/new", NewUser).Methods("POST")              // Declaramos el usuario
-	r.HandleFunc("/user/update/{id}", Updateuser).Methods("PATCH")  // Declaramos el usuario
-	r.HandleFunc("/user/delete/{id}", Deleteuser).Methods("DELETE")  // Declaramos el usuario
+	r.HandleFunc("/user/{id}", 		  GetUserRegisterRequest).Methods("GET")      // Declaramos el usuario
+	r.HandleFunc("/user/new", 		  NewUserRequest).Methods("POST")              // Declaramos el usuario
+	r.HandleFunc("/user/update/{id}", UpdateUserRequest).Methods("PATCH")  // Declaramos el usuario
+	r.HandleFunc("/user/delete/{id}", DeleteUserRequest).Methods("DELETE")  // Declaramos el usuario
 
-	log.Println("Servido en puerto 8000")                           // Indicamos que la apertura del servidor
+	log.Println("Servidor en puerto 8000")                           // Indicamos que la apertura del servidor
 	log.Fatal(http.ListenAndServe(":8000", r))                      // Levantando el servido
+	
 }
-
