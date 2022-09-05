3d6a68b7b6b3de7b4c5beb3cfde5a27f8dc837f5
jmachuca131281 <jmachucaprogrammer@gmail.com>
2022-09-05 11:10:30 -0500

    Added Handler

--- platzi/creacion_servidor_web/src/server.go
+++ platzi/creacion_servidor_web/src/server.go
@@ -10,12 +10,16 @@ type Server struct {
 func NewServer(port string) *Server {        // Permite instanciar el servidor. *Server hace referencia al valor que queremos verificar y no a copias.
 	return &Server{
 		port: port,
-		router: NewRouter()
+		router: NewRouter(),
 	}
 }
 
+func (s *Server) Handle(path string, handler http.HandlerFunc){
+	s.router.rules[path] = handler
+}
+
 func (s *Server) Listen() error {
-	http.Handle("/", &s.router)
+	http.Handle("/", s.router)
 	err := http.ListenAndServe(s.port, nil)
 	if err != nil {
 		return err
