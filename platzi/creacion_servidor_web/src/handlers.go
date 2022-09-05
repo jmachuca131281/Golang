609dbd28c9f8c1382bc5167f28be3956b3cb92a9
Jose Manuel Machuca Abreo <jmachuca@tfhkacol.local>
2022-09-05 15:50:50 -0500

    Se agrega type, middleware

--- platzi/creacion_servidor_web/src/handlers.go
+++ platzi/creacion_servidor_web/src/handlers.go
@@ -8,3 +8,7 @@ import (
 func HandleRoot(w http.ResponseWriter, r *http.Request){
 	 fmt.Fprintf(w, "HandleRoot")
 }
+
+func HandleHome(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Handle home")
+}
--- platzi/creacion_servidor_web/src/main.go
+++ platzi/creacion_servidor_web/src/main.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("/", HandleRoot)
+	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth()))
 	server.Listen()
 }
